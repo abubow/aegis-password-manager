a008e9b619a0c8cb38d7741e9a360045ba3fe07d
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-03-30 16:18:27 +0500

    rocket setup with hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![world])
 }
